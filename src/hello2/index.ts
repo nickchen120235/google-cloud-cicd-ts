e4a65b0e4d77e64dd0cce1b274401796d80b4dba
Nickchen Nick <nickchen120235@gmail.com>
2022-08-14 10:17:39 +0800

    test npm_package_version

--- src/hello2/index.ts
+++ src/hello2/index.ts
@@ -2,7 +2,7 @@ import type { HttpFunction } from '@google-cloud/functions-framework/build/src/f
 
 const hello2: HttpFunction = (req, res) => {
   if (req.method !== 'GET') return res.status(405).send(`${req.method} is not allowed.`)
-  res.status(200).send('Hello, World2!')
+  res.status(200).send(`Hello, World2! ${process.env.npm_package_version}`)
 }
 
 export default hello2
